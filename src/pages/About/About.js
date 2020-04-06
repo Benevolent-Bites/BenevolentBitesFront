35aab91729c9e6f871ac0562fd60737009383af4
Leo Orshansky <leoorshansky@gmail.com>
2020-04-06 12:40:58 -0500

    fixed about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -63,7 +63,7 @@ class About extends React.Component {
 
     async componentDidMount () {
         const content = await window.fetch(
-            process.env.PUBLIC_URL + "/about.html"
+            process.env.PUBLIC_URL + "/abouttext.html"
         ).then(result => result.text());
         document.getElementById("paper").innerHTML = content;
     }
